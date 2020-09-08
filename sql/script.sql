52b4ba00e96c18a915ffe90acbc5a3d2370d6cf2
victormarit <victormarit.95@gmail.com>
2020-09-08 12:53:31 +0200

    script BD fait

--- sql/script.sql
+++ sql/script.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE workshop;
-
-USE workshop;
+CREATE DATABASE workshop 
 
 CREATE TABLE utilisateur(
    id INT PRIMARY KEY  AUTO_INCREMENT,
@@ -12,11 +10,11 @@ CREATE TABLE categorie(
    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
    nom VARCHAR(50),
    consommation DECIMAL(15,2),
-   utilisationHebdo INT
+   utilisationHebdo INT 
 )ENGINE=INNODB;
 
 CREATE TABLE produitAlternative(
-   id INT,
+   id INT AUTO_INCREMENT,
    nom VARCHAR(50) NOT NULL UNIQUE,
    consommation INT,
    utilisationHebdomadaire INT,
@@ -34,7 +32,7 @@ CREATE TABLE possession (
     FOREIGN KEY(idCategorie) REFERENCES categorie(id)
 )ENGINE=INNODB;
 
-INSERT INTO produit (nom, consommation, utilisationHebdo)
+INSERT INTO produit (nom, consomation, utilisationHebdo)
 VALUES
 ('Congélateur', 350, 168),
 ('Réfrigérateur', 200, 168),
@@ -48,12 +46,11 @@ VALUES
 ('Four micro-ondes', 40, 1),
 ('Four classique', 365, 4),
 ('Ampoules', 110, 5),
-('Ampoules basses consommation', 22, 5);
+('Ampoules basses consoles', 22, 5);
 
-INSERT INTO utilisateur (email, password)
+insert into utilisateur (email, password)
 VALUES
 ('victormarit.95@gmail.com', '$2y$10$zaNn9Uz09ve.UHZ4X0NKCuSg0OsPa1R4TllBuYsMcQwia1CRldgua');
-
 
 
 INSERT INTO produitAlternative(nom, consommation, utilisationHebdomadaire, idCategorie)
@@ -62,8 +59,10 @@ VALUES
 ('WHIRLPOOL W7821OOX',204,168,2),
 (' ELECTROLUX EW7H4802SC', 309, 12, 3),
 ('Essentielb ELV-442b', 262, 12, 4),
-('',,,5),
-();
-
-
-
+('INDESIT BTWCA51052',148,10,5),
+('Toshiba U2963DG',96,21,6),
+('Essentielb EAS 73 HARDFLOOR',23,2,7),
+('ULTRA-PORTABLE WHY! N240WU 14',750,100,9),
+('PROLINE SM22',25,2,10),
+('Whirlpool EX AKZ9626IX',204,4,11),
+('Ampoule Philips LED 6,7 Watt E27',7,5,13);
