6b014fc9e517e659aa5bb75f92e0dc64e5c1f06e
Grégoire Hage <gregoire.hage@gmail.com>
2020-09-10 11:13:07 +0200

    correct colors & add alternatives

--- app/models/score.model.php
+++ app/models/score.model.php
@@ -27,3 +27,29 @@ function getScores($IdUser){
     }
     return $result;
 }
+
+function getAlternative(){
+    $result = [
+        "congelateur" => "inconnu",
+        "Aspirateur" => "inconnu",
+        "refrigerateur" => "inconnu",
+        "seche-linge" => "inconnu",
+        "Lave-vaisselle" => "inconnu",
+        "Lave-linge" => "inconnu",
+        "Television LCD" => "inconnu",
+        "Chargeur de telephone" => "inconnu",
+        "Ordinateur" => "inconnu",
+        "Four micro-ondes" => "inconnu",
+        "Four classique" => "inconnu",
+        "Ampoules" => "inconnu",
+        "Ampoules basses consoles" => "inconnu"
+    ];
+
+    $q = \Helpers\getDatabaseConnection()->prepare("SELECT produitalternative.nom, categorie.nom FROM produitalternative, categorie WHERE produitalternative.idCategorie = categorie.id");
+    $q->execute([]);
+    $tab = $q->fetchAll();
+    for ($i = 0; $i < count($tab); $i++){
+        $result[$tab[$i][1]] = $tab[$i][0];
+    }
+    return $result;
+}
