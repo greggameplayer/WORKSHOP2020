f26caeb77656467569f0809330ce5c83d537014e
jadDAOUK <jad190201.daouk@gmail.com>
2020-09-10 13:38:32 +0200

    questionnaire

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -1,8 +1,20 @@
-nameOfAllDevices=[];
-KhwOfAllDevices=[];
-endInfo=[];
-testeButtom=0;
-varchecked=0;
+var endInfo={
+    congelateur: [],
+    refrigerateur:[],
+    seche_linge:[],
+    Lave_vaisselle:[],
+    Lave_linge:[],
+    Television_LCD:[],
+    Aspirateur:[],
+    Ordinateur:[],
+    Four_micro_ondes:[],
+    Four_classique:[],
+    Ampoules:[]
+};
+var nameOfAllDevices = []
+var testeButtom=0;
+var varchecked=0;
+var medianePower = 0, medianeTime = 0;
 
 $("document").ready(function(){
     $(".firstChose").on("change",following);
@@ -21,6 +33,7 @@ function following(){
         varchecked+=1;
         $("#valider"+this.id).on("click",followfollowing);
     }else{
+        $("#wrapper--"+this.id).remove();
         varchecked-=1;
         let deleteDevice = this.id;
         for(let i = 0 ; i<nameOfAllDevices.length ; i++){
@@ -49,14 +62,14 @@ function followfollowing(){
         $("#wrapper--"+nameOfDevice).append('\
         <div class="underwrapper--'+nameOfDevice+'_'+numberofdevice+'">\
             <label for="thirdChose_'+nameOfDevice+numberofdevice+'_puissance"> La puissance de mon '+nameOfDevice+' n°'+numberofdevice+' est de </label>\
-            <input type="number" class="thirdChose" id="thirdChose_'+nameOfDevice+'_'+numberofdevice+'_puissance" step="1"  min="1" max="10000" required>\
+            <input type="number" class="thirdChose" id="thirdChose_'+nameOfDevice+'_'+numberofdevice+'_puissance" min="1" max="10000" required>\
             <label for="thirdChose_'+nameOfDevice+numberofdevice+'_puissance"> Watts.</label>\
             <label for="thirdChose_'+nameOfDevice+numberofdevice+'_temps"> Il fonctionne environs </label>\
-            <input type="number" class="thirdChose" id="thirdChose_'+nameOfDevice+'_'+numberofdevice+'_temps" step="1"  min="1" max="168" required>\
+            <input type="number" class="thirdChose" id="thirdChose_'+nameOfDevice+'_'+numberofdevice+'_temps" min="1" max="168" required>\
             <label for="thirdChose_'+nameOfDevice+numberofdevice+'_temps"> heure/semaine.</label>\
         </div>\
         ');
-        nameOfAllDevices.push(nameOfDevice);
+    nameOfAllDevices.push(nameOfDevice);      
     };
     if (testeButtom!=1){
         $(".main").append('\
@@ -68,14 +81,33 @@ function followfollowing(){
 }
 
 function ending(){
+    var testeMedianeTime=0;
+    var testeMedianePower=0;
     $(".thirdChose").each(function (){
-        if ($('['+this.id+'$=temps')){
-            var medianeTime=$(this).val();
+        if (this.id.includes("temps")){
+            medianeTime=this.value;
+            testeMedianeTime=1;
         }else{
-            var medianePower=$(this).val();
+            medianePower=this.value;
+            testeMedianePower=1;
         };
-        KhwOfAllDevices.push(medianeTime*medianePower/1000);
+        if(testeMedianePower==1&&testeMedianeTime==1){
+            endInfo[this.parentNode.parentNode.parentNode.children[0].id].push(medianeTime*medianePower/1000);
+            testeMedianeTime=0;
+            testeMedianePower=0;
+        }
     });
-    endInfo+=nameOfAllDevices+KhwOfAllDevices;
-    alert(endInfo);
+    for (let [key] of Object.entries(endInfo)) {
+        var allKwH = getArraySum(endInfo[key]);
+        endInfo[key]=allKwH/endInfo[key].length;
+    }
+    console.log(endInfo);
+}
+
+function getArraySum(a){
+    var total=0;
+    for(var i in a) { 
+        total += a[i];
+    }
+    return total;
 }
